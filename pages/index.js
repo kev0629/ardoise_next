ad4e47badc0b9461d23f42a36ecd7d28e99e0d1f
Kevin Saffioti <kevin.saffioti@gmail.com>
2021-09-08 19:53:08 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,41 @@
 import Head from 'next/head'
+import { google } from 'googleapis';
+
+export async function getServerSideProps({ query }) {
+
+  const auth = await google.auth.getClient({ scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly'] });
+
+  const sheets = google.sheets({ version: 'v4', auth });
+
+  const response = await sheets.spreadsheets.values.get({
+    spreadsheetId: process.env.SHEET_ID,
+    range: 'Feuille 1!A2:A10',
+  });
+
+  const posts = response.data.values.flat();
+  console.log(posts);
+
+  return {
+    props: {
+      posts,
+    },
+  };
+}
 
 
-export default function Home() {
+export default function Home({ posts }) {
   return (
     <div className="container">
       <div className="blackboard">
         <ul className="paper">
           <li><i className="fa fa-thumb-tack" aria-hidden="true"></i>
             <span><i className="fa fa-eraser" aria-hidden="true"></i></span>test</li>
+            {posts.map((posts) => (
+          <li key={posts}>
+            <i className="fa fa-thumb-tack" aria-hidden="true"></i>
+            <span><i className="fa fa-eraser" aria-hidden="true"></i></span>{posts}
+          </li>
+        ))}
         </ul>
       </div>
     </div>
