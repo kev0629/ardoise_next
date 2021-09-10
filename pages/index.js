132d1e0a85c45bbb7f8dd19e12db4609b33e21e0
Kevin Saffioti <kevin.saffioti@gmail.com>
2021-09-10 15:06:12 +0200

    working with notion

--- pages/index.js
+++ pages/index.js
@@ -1,44 +1,29 @@
 import Head from 'next/head'
-import { google } from 'googleapis';
-import  { GoogleSpreadsheet } from 'google-spreadsheet';
+import {Client} from '@notionhq/client'
 
-
-export async function getServerSideProps({ query }) {
-
-  const doc = new GoogleSpreadsheet(process.env.SHEET_ID);
-  await doc.useServiceAccountAuth({
-    client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-    private_key: process.env.GOOGLE_PRIVATE_KEY,
+export async function getStaticProps(){
+  const notion = new Client({ auth: process.env.NOTION_API_KEY });
+  const databaseId = process.env.DATABASE_ID;
+  const response = await notion.databases.query({
+    database_id: databaseId,
   });
-  // const auth = await google.auth.getClient({ scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly'] });
-
-  // const sheets = google.sheets({ version: 'v4', auth });
-
-  // const response = await sheets.spreadsheets.values.get({
-  //   spreadsheetId: process.env.SHEET_ID,
-  //   range: 'Feuille 1!A2:A10',
-  // });
-
-  // const posts = response.data.values.flat();
-  console.log(posts);
-
   return {
     props: {
-      posts,
+      results: response.results,
     },
   };
 }
 
 
-export default function Home({ posts }) {
+export default function Home({results}) {
   return (
     <div className="container">
       <div className="blackboard">
         <ul className="paper">
-            {posts.map((posts) => (
-          <li key={posts}>
+            {results.map((result) => (
+          <li key={results.indexOf(result)}>
             <i className="fa fa-thumb-tack" aria-hidden="true"></i>
-            <span><i className="fa fa-eraser" aria-hidden="true"></i></span>{posts}
+            <span><i className="fa fa-eraser" aria-hidden="true"></i></span>{result.properties.Name.title[0].plain_text}
           </li>
         ))}
         </ul>
