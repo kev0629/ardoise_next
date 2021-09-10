214a6e759780673de0ca25914f33a3e28d3cf7ee
Kevin Saffioti <kevin.saffioti@gmail.com>
2021-09-10 16:19:44 +0200

    add API and change from db

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,21 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+
+
+const { Client } = require('@notionhq/client');
+
+const notion = new Client({ auth: process.env.NOTION_API_KEY });
+
+
+
+
+export default async function handler(req, res) {
+
+
+  const response = await notion.databases.query({
+    database_id: process.env.DATABASE_ID,
+  });
+
+  res.status(200).json({ response })
 }
+
--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,40 @@
 import Head from 'next/head'
 import {Client} from '@notionhq/client'
 
-export async function getStaticProps(){
-  const notion = new Client({ auth: process.env.NOTION_API_KEY });
-  const databaseId = process.env.DATABASE_ID;
-  const response = await notion.databases.query({
-    database_id: databaseId,
-  });
-  return {
-    props: {
-      results: response.results,
-    },
-  };
-}
 
 
-export default function Home({results}) {
+
+export default function Home({datas}) {
+  console.log(datas)
   return (
     <div className="container">
       <div className="blackboard">
         <ul className="paper">
-            {results.map((result) => (
+          {datas.map(data => (
+            <li key={data.id}>{data.properties.Name.title[0].plain_text}</li>
+          ))}
+            {/* {results.map((result) => (
           <li key={results.indexOf(result)}>
             <i className="fa fa-thumb-tack" aria-hidden="true"></i>
             <span><i className="fa fa-eraser" aria-hidden="true"></i></span>{result.properties.Name.title[0].plain_text}
           </li>
-        ))}
+        ))} */}
         </ul>
       </div>
     </div>
   )
+}
+
+export async function getStaticProps (){
+  const notion = new Client({ auth: process.env.NOTION_API_KEY });
+  const databaseId = process.env.DATABASE_ID;
+  const response = await notion.databases.query({
+    database_id: process.env.DATABASE_ID,
+  });
+  return {
+    props: {
+      datas:response.results,
+    },
+    revalidate :1
+  };
 }
